7084e5acfb65bd413a54604a57204a4e469e3c03
cesar montalvan <ces1406@gmail.com>
2020-12-08 10:26:25 -0300

    create front-end launcher (webpack + babel)

---
+++ front-end/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlPlugin = new HtmlWebpackPlugin({
+    template:'./static_files/index.html',
+    filename:'../dist/index.html'
+})
+module.exports={
+    /*entry:'./src/index.js',
+    output:{
+        filename:'bundle.[hash].js',
+        path:path.resolve(__dirname,'dist')
+    },
+    mode:'production',*/
+    module:{
+        rules:[
+            {
+                test:/\.(js|jsx)$/,
+                use:['babel-loader'],
+                exclude:/node_modules/,
+            }
+        ]
+    },
+    plugins:[HtmlPlugin]
+}
